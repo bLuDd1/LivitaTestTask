a69f0f4863001ae66f26b98348a2e0b3e14ffcf0
Dmytro Tavlui <dtavluj@gmail.com>
2026-06-18 00:40:00 +0300

    Initial Commit

---
+++ LivitaTestTask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LivitaTestTask
+//
+//  Created by Dima on 18.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LivitaTestTask/LivitaTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  LivitaTestTaskApp.swift
+//  LivitaTestTask
+//
+//  Created by Dima on 18.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct LivitaTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
